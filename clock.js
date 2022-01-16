b05c19f5a2eac54c7d09ec0616462f42d6f594b9
leejanghe <babybye777@gmail.com>
2022-01-17 02:32:34 +0900

    clock done

--- clock.js
+++ clock.js
@@ -1,3 +1,24 @@
 const clock = document.querySelector('h2#clock');
 
-clock.innerText = new Date().toLocaleTimeString();
+
+
+//setInterval은 인자로 주어진 함수를 주어진 시간마다 반복실행하는 함수
+
+// 깔끔한 방법
+function sayHello(){
+    clock.innerText = new Date().toLocaleTimeString();
+}
+sayHello()
+setInterval(sayHello, 1000);
+
+
+// 두번째 방법 padStart (첫번째 인자는 길이, 두번째 인자는 채울 문자)
+function sayHello(){
+    const data = new Date();
+    const hours =  String(data.getHours()).padStart(2,"0")
+    const minutes = String(data.getMinutes()).padStart(2,"0")
+    const seconds = String(data.getSeconds()).padStart(2,"0")
+    clock.innerText = `${hours}:${minutes}:${seconds}`
+}
+sayHello()
+setInterval(sayHello, 1000);
