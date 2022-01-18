e385f2f2f6f08e8fa29026216bc94409bb0aebb1
leejanghe <babybye777@gmail.com>
2022-01-19 00:51:21 +0900

    todo delete

--- js/todolist.js
+++ js/todolist.js
@@ -3,6 +3,8 @@ const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = 'todos';
+
+// scope를 위해 전역변수로 선언
 let toDos = [];
 
 function saveToDos(){
@@ -14,14 +16,18 @@ function saveToDos(){
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove()
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id))
+    saveToDos();
 }
 
 
 // append는 마지막에 추가해야 합니다.
+// todolist 제거하기 위해 id값 부여
 function paintToDo(newToDo){
     const liTag = document.createElement('li');
+    liTag.id = newToDo.id;
     const spanTag = document.createElement('span')
-    spanTag.innerText = newToDo;
+    spanTag.innerText = newToDo.text;
 
     const button = document.createElement('button')
     button.addEventListener('click', deleteToDo)
@@ -35,14 +41,19 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = '';
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    // storge에 저장된 값을 삭제하기 위해 객체로 만든다.
+    const newToDoObj ={
+        text:newTodo,
+        id:Date.now(),
+    }
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-
+// local storge에 저장
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 //  json.parse는 문자열을 객체로 변환하는 함수
@@ -52,3 +63,4 @@ if(savedToDos !== null){
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo)
 }
+
